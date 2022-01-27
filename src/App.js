4807d616e2a0f2cd838bd46d25c6d90e845d8c1b
Laxmi Ittappa <laxmigowthami9@gmail.com>
2022-01-27 15:48:58 +0530

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -21,11 +21,21 @@ constructor() {
 unsubscribeFromAuth = null;
 
 componentDidMount() {
-  this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-    this.setState({currentUser: user});
+  this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    if(userAuth) {
+      const userRef = await createUserProfileDocument(userAuth);
 
-    console.log(user);
-  })
+      userRef.onSnapshot(snapShot => {
+        this.setState({
+          currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+        });
+      });
+    }
+    this.setState({currentUser: userAuth});
+  });
 }
 
 componentWillUnmount() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,31 @@ const config = {
     measurementId: "G-LE9V6V2J24"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+        
+        try {
+            await userRef.set({
+                    displayName,
+                    email,
+                    createdAt,
+                    ...additionalData
+        })
+        }
+        catch (error) {
+            console.log("error creating user :", error);
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
